c2faafcb77911af320766fb981ea4c2d0954e9a2
ElMisanteco <elmisanteco@yahoo.com>
2025-02-11 14:25:36 -0600

    Implementación del 2 de 3

--- main.rb
+++ main.rb
@@ -1,36 +1,78 @@
-# Opciones disponibles para elegir.
-opciones = ["piedra", "papel", "tijera"]
 
-begin # ----> Loop que se repetirá hasta que el usuario ingrese una opción válida.
+def piepaptij()
 
-  # Solicitar al usuario que escoja una de las opciones y a continuación se captura.
-  puts "¿Piedra papel o tijera?"
-  elec_usuario = gets.chomp.downcase
+  # Opciones disponibles para elegir.
+  opciones = ["piedra", "papel", "tijera"]
 
-# Si lo que ingresó el usuario no es válido, se entra en el if y se indica al usuario que lo que ingresó no es válido.
-unless opciones.include?(elec_usuario)
-  puts "--> #{elec_usuario} <-- no es válido"
+  begin # ----> Loop que se repetirá hasta que el usuario ingrese una opción válida.
+
+    # Solicitar al usuario que escoja una de las opciones y a continuación se captura.
+    puts "¿Piedra papel o tijera? 😀"
+    elec_usuario = gets.chomp.downcase
+
+  # Si lo que ingresó el usuario no es válido, se entra en el if y se indica al usuario que lo que ingresó no es válido.
+  unless opciones.include?(elec_usuario)
+    puts "--> #{elec_usuario} <-- no es válido. 🤨"
+  end
+
+  end until opciones.include?(elec_usuario) # ---> El loop terminará si la elección que ingresó el usuario es válida.
+
+  # Eleccion de la computadora.
+  elec_pc = opciones.sample
+
+  case
+    when elec_usuario == elec_pc
+      puts "Tú escogiste #{elec_usuario} y la computadora #{elec_pc}."
+      puts "Tenemos un empate. 😮"
+
+    when elec_usuario == "piedra" && elec_pc == "tijera"
+      puts "Tú escogiste #{elec_usuario} y la computadora #{elec_pc}."
+      puts "🎉🎊🏆 Felicidades por tu victoria. 🏆🎊🎉"
+      victoria = "usuario"
+
+    when elec_usuario == "papel" && elec_pc == "piedra"
+      puts "Tú escogiste #{elec_usuario} y la computadora #{elec_pc}."
+      puts "🎉🎊🏆 Felicidades por tu victoria. 🏆🎊🎉"
+      victoria = "usuario"
+
+    when elec_usuario == "tijera" && elec_pc == "papel"
+      puts "Tú escogiste #{elec_usuario} y la computadora #{elec_pc}."
+      puts "🎉🎊🏆 Felicidades por tu victoria. 🏆🎊🎉"
+      victoria = "usuario"
+
+    else
+      puts "Tú escogiste #{elec_usuario} y la computadora #{elec_pc}."
+      puts "😿 Diablos, que mala suerte, perdiste, lo siento.😔 😿"
+      victoria = "computadora"
+
+  end
+
+  return victoria
+
 end
 
-end until opciones.include?(elec_usuario) # ---> El loop terminará si la elección que ingresó el usuario es válida.
 
-# Eleccion de la computadora.
-elec_pc = opciones.sample
 
-case
-  when elec_usuario == elec_pc
-    puts "Tú escogiste #{elec_usuario} y la computadora #{elec_pc}."
-    puts "Tenemos un empate. 😮"
-  when elec_usuario == "piedra" && elec_pc == "tijera"
-    puts "Tú escogiste #{elec_usuario} y la computadora #{elec_pc}."
-    puts "🎉🎊🏆 Felicidades por tu victoria. 🏆🎊🎉"
-  when elec_usuario == "papel" && elec_pc == "piedra"
-    puts "Tú escogiste #{elec_usuario} y la computadora #{elec_pc}."
-    puts "🎉🎊🏆 Felicidades por tu victoria. 🏆🎊🎉"
-  when elec_usuario == "tijera" && elec_pc == "papel"
-    puts "Tú escogiste #{elec_usuario} y la computadora #{elec_pc}."
-    puts "🎉🎊🏆 Felicidades por tu victoria. 🏆🎊🎉"
-  else
-    puts "Tú escogiste #{elec_usuario} y la computadora #{elec_pc}."
-    puts "😿 Diablos, que mala suerte, pediste, lo siento.😔 😿"
+victoria_total_pc = 0
+victoria_total_usuario = 0
+
+begin
+
+  ganador = piepaptij()
+
+  if ganador == "computadora"
+    victoria_total_pc = victoria_total_pc +1
+  elsif ganador == "usuario"
+    victoria_total_usuario = victoria_total_usuario + 1
+  end
+
+  puts "Usuario: #{victoria_total_usuario}"
+  puts "Computadora:  #{victoria_total_pc}"
+
+end until victoria_total_pc == 2 || victoria_total_usuario == 2
+
+if victoria_total_pc < victoria_total_usuario
+  puts "🎉🥳 🏆Felicidades, Tú eres el ganador🏆 🥳🎉"
+else
+ puts "😿🤙 Perdiste, que mala suerte 😿🤙"
 end
